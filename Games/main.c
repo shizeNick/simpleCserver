03006764f9c93d9d2e037d03eb62b303a029982c
rainvi <nick.schmidt1711@gmail.com_Laptop>
2025-06-27 09:46:52 +0200

    logic fix

--- Games/main.c
+++ Games/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h> // for free
+#include <sys/wait.h>
 
 
 char* show();
@@ -16,10 +17,10 @@ int main(){
     choose_name();
     init_ground();
     // game intro preview
-    char* board = preview();
-    if (board != NULL) { // Immer gut, NULL-Check zu machen
-        printf("\n%s", board);
-        free(board); // Speicher freigeben
+    char* preview_board = preview();
+    if (preview_board != NULL) { // Immer gut, NULL-Check zu machen
+        printf("\n%s", preview_board);
+        free(preview_board); // Speicher freigeben
     }
     
     int rounds = 1; 
@@ -35,18 +36,43 @@ int main(){
         printf("%s", board);
         if(rounds % 2 != 0){
             int i;
+            int wait = 1;
+            int check;
             printf("%s ist am Zug!\n", player_1);
-            scanf("%i", &i);
-            move(i, 1);
-            rounds++;
+            while(1){
+                check = scanf("%i", &i);
+                move(i, 1);
+                //Puffer leeren, egal ob scanf erfolgreich war oder nicht
+                while ((getchar()) != '\n' && getchar() != EOF);
+                if(check == 1){
+                    if((i > 1) || (i < 9)){
+                        break;
+                    }
+                }
+            }// Wichtig: Leere den Eingabepuffer!
+            // Lies und verwerfe alle Zeichen im Puffer bis zum nächsten Newline-Zeichen oder EOF
+                rounds++;
         }else {
             int i;
+            int wait = 1;
+            int check;
             printf("%s ist am Zug!\n", player_2);
-            scanf("%i", &i);
-            move(i, 2);
-            rounds++;
+            while(1){
+                check = scanf("%i", &i);
+                move(i, 2);
+                //Puffer leeren, egal ob scanf erfolgreich war oder nicht
+                while ((getchar()) != '\n' && getchar() != EOF);
+                if(check == 1){
+                    if((i > 1) || (i < 9)){
+                        break;
+                    }
+                }
+            }
+                rounds++;
         }
     }
+    char* win_board = show();
+    printf("\n%s", win_board);
     if(winner == 1){
         printf("~~~ %s hat Gewonnen! ~~~", player_1);
     }else if(winner == 2){
